7f5418185dba4f271704e7e134f7446294b168d1
Jeffrey Na <jeffreyna@jeffreys-air.myfiosgateway.com>
2023-05-16 19:42:11 -0400

    Added userController to createUser and verifyUser

---
+++ server/controllers/userController.js
@@ -0,0 +1,45 @@
+const User = require('../models/userModel');
+const bcrypt = require('bcrypt'); 
+
+const userController = {};
+
+userController.createUser = async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    const foundUser = await User.findOne({ username: username });
+    if (foundUser) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      await User.create({ username, hashedPassword });
+      return next();
+    } else {
+      // **CHECK**: when user signs up with already used username  
+      return res.redirect('/signup');
+    }
+  } catch (err) {
+    return next({
+      log: 'Error creating new user -- username and/or password not provided',
+      status: 400,
+      message: { err: 'Username or password invalid.' }
+    });
+  }
+};
+
+userController.verifyUser = async (req, res, next) => {
+  const { username, password } = req.body;
+
+  try {
+    const foundUserInfo = await User.findOne({ username: username });
+    console.log(foundUserInfo);
+    const correctPW = await bcrypt.compare(password, foundUserInfo.password);
+    res.locals.verified = correctPW;
+    return next();
+  } catch {
+    return next({
+      log: 'Error logging in -- username and/or password not provided',
+      status: 400,
+      message: { err: 'Username or password invalid.' }
+    });
+  }
+};
+
+module.exports = userController;
