cb40d314dd51517261c5bc73aa9840b885f6f2a0
Jeffrey Na <jeffreyna@jeffreys-air.myfiosgateway.com>
2023-05-17 11:20:12 -0400

    server.js cookie parser

--- server/server.js
+++ server/server.js
@@ -13,10 +13,7 @@ const port = process.env.PORT || 3000;
 const MONGO_URI =
 'mongodb+srv://jeff-user:OhcSk2i7cb6bNJU7@cluster0.5z6r4sx.mongodb.net/?retryWrites=true&w=majority';
 
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(MONGO_URI)
   .then(() => console.log('Connected to Mongo DB.'))
   .catch(err => console.log(err));
 
